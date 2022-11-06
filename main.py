71938c164e49278ff1e67cd3a0892d70b15be7db
Whiffy <whiffy@example.com>
2022-11-06 17:24:53 -0500

    update maine

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from tqdm import tqdm
 def main():
     args = get_args()
     input_path = os.path.expanduser(args.input)
-    print(os.listdir(input_path))
-    for file in tqdm(os.listdir(input_path)):
-        print(input_path + "/" + file, rename(input_path, file))
-    print("Hello World!")
+    # print(os.listdir(input_path))
+    for path, subdirs, files in os.walk(input_path):
+        for subdir in subdirs:
+            for file in os.listdir(os.path.join(path, subdir)):
+                print(rename(subdir, file))
+    # for file in tqdm(os.listdir(input_path)):
+    # print(input_path + "/" + file, rename(input_path, file))
 
 
 def get_args():
